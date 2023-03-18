bd20d30299ecd13317fccfe3b3748dc1b583f9a4
Olena Denysenko <helenazinchenko@ukr.net>
2023-03-18 23:32:33 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { GlobalStyle } from 'GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-    // basename="/goit-react-hw-05-movies/"
-    >
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
       <GlobalStyle />
     </BrowserRouter>
